8b37faed0f826db777f5e3db88b96f11ab99c666
Dan Gil <dan.nek0.bass@gmail.com>
2019-08-29 14:05:52 -0500

    start button added and works

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let notes = [];
 let frames = 0;
 let current;
 let ticks = 0;
+let score = 0;
+let interval;
+//let board;
 
 class Board {
   constructor() {
@@ -21,41 +24,48 @@ class Board {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
+let board = new Board();
 
 class Note {
   constructor(options) {
     this.x = canvas.width;
     this.y = options.y;
+    this.key = options.key;
     this.color = options.color;
   }
   draw() {
-    this.x--;
+    this.x -= 2;
     //ctx.drawImage(this.quarterNote, this.x, this.y, this.width, this.height);
+    ctx.save();
     ctx.font = '120px Metdemo';
     ctx.fillStyle = this.color;
     ctx.fillText('Q', this.x, this.y);
+    ctx.restore();
   }
 }
 
 function generateNotes() {
   const notas = [
-    { y: 363, color: '#cf0000' },
-    { y: 337, color: '#ff8000' },
-    { y: 315, color: '#1789FC' },
-    { y: 289, color: '#9955ff' },
-    { y: 263, color: '#06d444' },
-    { y: 237, color: '#E0479E' },
-    { y: 207, color: '#ffe203' },
-    { y: 181, color: '#cf0000' },
-    { y: 155, color: '#ff8000' },
-    { y: 129, color: '#1789FC' },
-    { y: 105, color: '#9955ff' },
-    { y: 75, color: '#06d444' }
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 337, color: '#ff8000', key: 'd' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 289, color: '#9955ff', key: 'f' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 237, color: '#E0479E', key: 'a' },
+    { y: 207, color: '#ffe203', key: 'b' },
+    { y: 181, color: '#cf0000', key: 'c' },
+    { y: 155, color: '#ff8000', key: 'd' },
+    { y: 129, color: '#1789FC', key: 'e' },
+    { y: 105, color: '#9955ff', key: 'f' },
+    { y: 75, color: '#06d444', key: 'g' }
   ];
+  const idx = Math.floor(Math.random() * notas.length);
 
-  if (frames % 200 === 0) {
-    const idx = Math.floor(Math.random() * notas.length);
+  if (frames % 70 === 0) {
     notes.push(new Note(notas[idx]));
+  }
+  if (notes.length >= 10) {
+    notes.shift(new Note(notas[idx]));
   }
 }
 
@@ -67,58 +77,155 @@ function drawNotes() {
 
 function update() {
   window.requestAnimationFrame(update);
-
+  //se puede settear un time para llamar requestAnim.. con tiempo más corto
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   frames++;
+  //console.log(notes);
   board.draw();
   generateNotes();
   drawNotes();
+  // easySong1();
+  // drawEasySong();
+  deleteNotes();
+  drawScore();
+}
+
+function getCurrent() {
+  let [note] = notes;
+  if (note && note.x < 230) {
+    return note.key;
+  }
+}
+
+function deleteNotes() {
+  let [note] = notes;
+  if (note && note.x < 136) {
+    notes.shift();
+  }
 }
 
 function handleButtonClick(e) {
   keyPressed(e.target.dataset.key);
 }
 
-function playAudio(url) {
-  let audio = new Audio(url);
+function playAudio(id) {
+  let audio = document.getElementById(id);
   audio.play();
+  notes.shift();
 }
 
-const board = new Board();
-
-window.addEventListener('load', update);
-
-document.querySelectorAll('button').forEach(element => {
-  element.addEventListener('click', handleButtonClick);
-});
-
-window.addEventListener('keydown', e => {
-  keyPressed(e.key);
-});
-
+function drawScore() {
+  let elmnt = document.getElementById('score');
+  elmnt.innerText = score;
+  return;
+  // ctx.save();
+  // ctx.font = '24px Courier';
+  // ctx.fillStyle = 'white';
+  // ctx.fillText(score, canvas.width / 2, 50);
+  // ctx.restore();
+}
+function stop() {
+  clearInterval(interval); // detiene el intervalo
+  interval = null; // se reasigna el valor de interval
+}
 function keyPressed(key) {
+  console.log(key, getCurrent());
+  if (key !== getCurrent()) {
+    return;
+  }
+  score += 10;
   switch (key) {
     case 'c':
+      playAudio('do');
       break;
     case 'd':
+      playAudio('re');
       break;
     case 'e':
+      playAudio('mi');
       break;
     case 'f':
+      playAudio('fa');
       break;
     case 'g':
+      playAudio('sol');
       break;
     case 'a':
+      playAudio('la');
       break;
     case 'b':
+      playAudio('si');
       break;
-
+    case 81:
+      stop();
     default:
       break;
   }
   console.log(key);
 }
+function start() {}
+window.addEventListener('load', update);
+window.addEventListener('keydown', e => {
+  keyPressed(e.key);
+});
+
+document.querySelectorAll('button').forEach(element => {
+  element.addEventListener('click', handleButtonClick);
+});
 
 addEventListener('click', e => {
   console.log(e);
 });
+
+document.getElementsByClassName('start-game-button')[0].onclick = function() {
+  //hace un array de elementos que contengan esa clase y selecciono el primero
+  document.querySelector('.start-game-button').style.display = 'none';
+  start();
+  //  document.querySelector('#staff').style.display = 'show';
+};
+
+/*
+function easySong1() {
+  let martinillo = [
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 337, color: '#ff8000', key: 'd' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 337, color: '#ff8000', key: 'd' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 289, color: '#9955ff', key: 'f' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 289, color: '#9955ff', key: 'f' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 237, color: '#E0479E', key: 'a' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 289, color: '#9955ff', key: 'f' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 363, color: '#cf0000', key: 'c' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 237, color: '#E0479E', key: 'a' },
+    { y: 263, color: '#06d444', key: 'g' },
+    { y: 289, color: '#9955ff', key: 'f' },
+    { y: 315, color: '#1789FC', key: 'e' },
+    { y: 363, color: '#cf0000', key: 'c' }
+  ];
+
+  if (frames % 70 === 0) {
+    martinillo.push(new Note());
+  }
+  if (martinillo.length >= 10) {
+    martinillo.shift(new Note());
+  }
+}
+
+function drawEasySong() {
+  martinillo.forEach(note => {
+    note.draw();
+  });
+}
+*/
